05e57ed1406c776b65183f2991eb3785c326d49d
drmitia <drdrmitia@gmail.com>
2024-04-13 22:51:39 +0300

    6homework

---
+++ EpamHW6/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamHW6
+{
+    internal class Car
+    {
+        public string name;
+        public int max_velocity;
+        public string Name
+        { 
+            get { return name; }  
+            set { name = value; }
+        }
+        public int Max_velocity
+        {
+            get { return max_velocity; }
+            set { max_velocity = value; }
+        }
+        public Car(string carName,int carVelocity)
+        { 
+            Name = carName;
+            Max_velocity = carVelocity;
+        }
+    }
+}
---
+++ EpamHW6/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EpamHW6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Honda = new Car("Honda Civic", 300);
+            Car Mercedes = new Car("Mercedes Sprinter", 320);
+            if (Honda.Max_velocity > Mercedes.Max_velocity)
+            {
+                Console.WriteLine($"Найшвидша машина {Honda.Name}");
+            }
+            else if (Mercedes.Max_velocity > Honda.Max_velocity) 
+            {
+                Console.WriteLine($"Найшвидша машина {Mercedes.Name}");
+            }
+            else 
+            {
+                Console.WriteLine("Визначити найшвидшу машину неможливо");
+            }
+        }
+    }
+}
